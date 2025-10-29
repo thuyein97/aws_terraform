66e7f9eef128374505a7f88d877a58c739cb4152
ARS - Thu Yein Tin <111072736+thuyein97@users.noreply.github.com>
2025-10-29 14:27:31 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ provider "aws" {
 
 variable "storage_account_name" {
   type = string
-  description = "name of the storage account"
 }
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
